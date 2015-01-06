d64fb2c8f17834c5105a9927f6a7fab0ea3a7a21
Robin Beckett <subwolf@gmail.com>
2015-01-06 17:31:21 -0500

    Total rewrite.

--- SlackBot.py
+++ SlackBot.py
@@ -4,103 +4,40 @@
 # Storj Bot for Slack (with other functions)
 
 # Imports
-import os
-import sys
-import time
+import os, sys, time, socket
+import ssl, logging, json
 import threading
-import json
 
-from SlackCore import SlackInteractor
+import BaseHTTPServer
+import SocketServer
+
+from BotInfo import botData
 from SlackCore import SlackRTM
 from SlackCore import PostHandler
-from BotInfo import botData
-import BaseHTTPServer, SimpleHTTPServer
-import ssl
 
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+logger = logging.getLogger('SlackBot')
+logger.setLevel(logging.DEBUG)
+ch = logging.FileHandler(botData.log_file, 'a', None, False)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+logger.info("Initializing...")
+
 server        = BaseHTTPServer.HTTPServer((botData.listen_ip, botData.listen_port), PostHandler)
-server.socket = ssl.wrap_socket (server.socket, certfile=botData.key_file, server_side=True)
+server.socket = ssl.wrap_socket (sock=server.socket, certfile=botData.ssl_cert_file, 
+                keyfile=botData.ssl_key_file, server_side=True, ca_certs="data/ca.crt")
+                
 print 'Starting server, use <Ctrl-C> to stop'
-server.serve_forever()
 
-def RTMRunner():
-	rtm    = SlackRTM()
-	while True:
-		rtm.ActivityCheck()
+try:
+	server.serve_forever()
+except KeyboardInterrupt:
+        server.server_close()
+print "Server Stopped - %s:%s" % (botData.listen_ip, botData.listen_port)
 
-def ClientRunner():
-	client = SlackInteractor()
-	client.SetupJson()
-	client.CheckMessages(100)
-	client.SaveJson()
-	print "- Initial Channel History Scan Complete."
-	while True:
-		if client.CheckMessages(10) > 0:
-			client.SaveJson()
-			print "Time to output new template..."
-			client.OutputTemplate()
-			client.newEntries = 0
-			time.sleep(2)
-		else:
-			# print "No new template needed."
-			client.CheckRefresh()
-			time.sleep(2)
-
-def doTemplate():
-	client = SlackInteractor()
-	client.SetupJson()
-	print "Outputting new template..."
-	client.OutputTemplate()
-	print "... done."
-	sys.exit()
-
-def doRun():
-	print "Starting SlackBot..."
-	a = threading.Thread(target=ClientRunner)
-	a.daemon=True
-	a.start()
-
-	time.sleep(5)
-
-	b = threading.Thread(target=RTMRunner)
-	b.daemon=True
-	b.start()
-
-	while True:
-		time.sleep(1) # Do nothing
-
-def doDump():
-	status_in = open(botData.status_file, "r")
-	botJson   = json.load(status_in)
-	status_in.close()
-
-	print json.dumps(botJson, indent=4)
-
-def doTest():
-	print "Testing..."
-	test  = SlackInteractor()
-	test.SetupJson()
-	test.SetAdmins()
-	
-args = len(sys.argv)
-if args > 1:
-	arg = sys.argv[1]
-	if arg == "template":
-		doTemplate()
-	elif arg == "dump":
-		doDump()
-	elif arg == "test":
-		doTest()
-	else:
-		doRun()
-else:
-	doRun()
-	
-'''
-
-U02FSFYNG
-
-'''
 
--- SlackCore/__init__.py
+++ SlackCore/__init__.py
@@ -37,25 +37,58 @@ lock = threading.Lock()
 # based on http://pymotw.com/2/BaseHTTPServer/
 
 class PostHandler(BaseHTTPRequestHandler):
-    
-    def do_POST(self):
-    	global rtm
-        form = cgi.FieldStorage(
-            fp=self.rfile, 
-            headers=self.headers,
-            environ={'REQUEST_METHOD':'POST',
-                     'CONTENT_TYPE':self.headers['Content-Type'],
-                     })
 
-        self.send_response(200)
-        self.end_headers()
+	def setup(self):
+		self.connection = self.request
+		#self.rfile = socket._fileobject(self.request, "rb", self.rbufsize)
+		#self.wfile = socket._fileobject(self.request, "wb", self.wbufsize)
+		self.rfile = self.connection.makefile('rb', self.rbufsize)
+		self.wfile = self.connection.makefile('wb', self.wbufsize)
+        
+	def do_POST(self):
+		reqvars = ['token', 'team_id', 'channel_id', 'channel_name',
+			   'timestamp', 'user_id', 'user_name', 'text',
+			   'trigger_word']
 
-	# Send it onto our parser
-	rtm      = SlackRTM(False) # Init without socket
-	response = rtm.parse(form)
-	if len(response) > 0:
-		self.wfile.write(response)
-	return
+		try:
+			logger = logging.getLogger("SlackBot")
+			logger.info("Got POST data")
+			form = cgi.FieldStorage(
+			    fp=self.rfile, 
+			    headers=self.headers,
+			    environ={'REQUEST_METHOD':'POST',
+				     'CONTENT_TYPE':self.headers['Content-Type'],
+				     })
+
+			self.send_response(200)
+			self.end_headers()
+		except:
+			traceback.print_exc(file=sys.stdout)
+			
+		# Test we got everything
+		jsonData = {}
+		for field in form.keys():
+			jsonData[field] = form[field].value.strip()
+		
+		for idx, val in enumerate(reqvars):
+			if val not in jsonData:
+				response = "Incorrect data. Please try again!"
+				self.wfile.write(response)
+				return
+
+		logger.info("Received: " + json.dumps(jsonData, indent=4))
+
+		rtm      = SlackRTM(False) # Init without socket
+		response = rtm.Parse(jsonData)
+		logger.info(response)
+		self.wfile.write(json.dumps(response))
+		return
+
+	def finish(self):
+		if not self.wfile.closed:
+			self.wfile.flush()
+		self.wfile.close()
+		self.rfile.close()
 
 
 class SlackRTM(object):
@@ -124,23 +157,23 @@ class SlackRTM(object):
 		return 0
 
 			
-	def Parse(self, form):
-		# Turn it into json
-		jsonData = {}
-		for field in form.keys():
-			jsonData[field] = form[field].value.strip()
-			
+	def Parse(self, jsonData):
+		logger = logging.getLogger("SlackBot")
+
 		# Verify Token
 		if jsonData['token'] != botData.hook_token:
 			return "Token from Slack does not match ours, ignoring."
 			
+		logger.debug("Looking through hooks for a match...")
 		for key, val in self.triggers.iteritems():
 			test = self.triggers[key].match(jsonData['text'])
 			if test:
-				self._Process(re.findall(self.triggers[key], jsonData['text']), jsonData)
+				logger.debug("Got a match with: " + self.hooks[key])
+				return self._Process(re.findall(self.triggers[key], jsonData['text']), jsonData)
 
 			
 	def _Process(self, matchData, jsonData):
+		logger = logging.getLogger("SlackBot")
 		response = ""
 
 		# Ok, lets go through our list...
@@ -150,23 +183,22 @@ class SlackRTM(object):
 			if k == ".markets":
 				response  = "Melotic SJCX/BTC: " + str(self.GetExRate("melotic")) + "\n"
 				response += "Poloniex SJCX/BTC: " + str(self.GetExRate("poloniex"))
+				return response
 			elif k == ".melotic":
-				response  = "Melotic SJCX/BTC: " + str(self.GetExRate("melotic"))
+				return "Melotic SJCX/BTC: " + str(self.GetExRate("melotic"))
 			elif k == ".poloniex":
-				response  = "Poloniex SJCX/BTC: " + str(self.GetExRate("poloniex"))
+				return "Poloniex SJCX/BTC: " + str(self.GetExRate("poloniex"))
 			elif k == ".undo":
 				self.SetupJson()
 				self.botJson['updates'][jsonData['user_id']] = self.botJson['undo'][jsonData['user_id']]
 				self.SaveJson()
 				self.OutputTemplate()
 				response = "<@" + jsonData['user_id'] + ">: I have undone your last update and refreshed the template."
+				return response
 			elif k == ".regen":
 				self.SetupJson()
 				self.OutputTemplate()
-				response = "<@" + jsonData['user_id'] + ">: I have refreshed the template."
-			
-			if len(response) > 0:
-				return response
+				return "<@" + jsonData['user_id'] + ">: I have refreshed the template."
 			else:
 				return "No response to give!"
 				
@@ -177,18 +209,14 @@ class SlackRTM(object):
 			
 			if k == ".balance":
 				balance = str(self.GetBalance(v))
-				response = "Balance for address " + v + ": " + balance + " SJCX"
+				return "Balance for address " + v + ": " + balance + " SJCX"
 			elif k == ".twitter":
 				self.SetupJson()
 				self.botJson['twitter'][jsonData['user_if']] = v
 				self.SaveJson()
-				response = "<@" + jsonData['user_id'] + ">: Your twitter handle is now '" + v + "'."
-				
-			if len(response) > 0:
-				return response
+				return "<@" + jsonData['user_id'] + ">: Your twitter handle is now '" + v + "'."
 			else:
 				return "No response to give!"
-
 		else:
 			return "No response to give!"
 							
