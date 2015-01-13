50416a0e38095c8edf40dbae3614feae27d4c40b
Robin Beckett <subwolf@gmail.com>
2015-01-12 19:10:07 -0500

    Temporarily broken. Trying to fix issues with parsing requests.

--- SlackCore/__init__.py
+++ SlackCore/__init__.py
@@ -6,7 +6,7 @@
 
 # Imports
 import os, sys, traceback, json, simplejson, time, re, logging, ssl
-import websocket, requests, datetime, cgi
+import websocket, requests, datetime, cgi, urllib, hashlib
 
 from BaseHTTPServer import BaseHTTPRequestHandler
 import SocketServer
@@ -71,13 +71,16 @@ class PostHandler(BaseHTTPRequestHandler):
 class SlackResponder(object):
 	# Messages we understand and parse
 	hooks = {
-		"status" : 	"^(\.status) (.*)$",
-		"balance" :	"^(\.balance) ([A-Za-z0-9]{25,36})$",
-		"twitter" : 	"^(\.twitter) ([\w]+)$",
 		"add":    	"^(\.add) \<\@([\w]+)\> ?(admin|superuser)?",
 		"del":		"^(\.del) \<\@([\w]+)\>",
 		"hide":		"^(\.hide) \<\@([\w]+)\>",
 		"show":		"^(\.show) \<\@([\w]+)\>",
+		"twitter": 	"^(\.twitter) ([\w]+)$",
+		"email":        "^(\.email) ([\w]+)$",
+		"image":	"^(\.image) ([\w]+)$",
+		"name":		"^(\.name) (.*)$",
+		"status" : 	"^(\.status) (.*)$",
+		"balance" :	"^(\.balance) ([A-Za-z0-9]{25,36})$",
 		"list":		"^\.list",
 		"markets" :	"^\.markets",
 		"melotic" :	"^\.melotic",
@@ -121,7 +124,6 @@ class SlackResponder(object):
 			status_in = open(botData.status_file, "r")
 			data      = status_in.read()
 			status_in.close()
-			logger.debug("Length: " + str(len(data)) + " characters.")
 			self.botJson = simplejson.loads(data)
 		else:
 			# Starting over
@@ -130,6 +132,7 @@ class SlackResponder(object):
 				"updates": [],
 				"twitter": {},
 				"undo": {},
+			        "email": {},
 			        "admins": [botData.owner_id],
 			        "superusers": [botData.owner_id],
 			        "hidden": []
@@ -169,7 +172,8 @@ class SlackResponder(object):
 			
 	def _Process(self, postData, matchText):
 		logger = logging.getLogger("SlackBot")
-		self.SetupJson()
+		self.SetupJson() # Needed for comparison checks
+		# logger.debug(json.dumps(postData, indent=4))
 
 		try:
 			del postData['token'], postData['trigger_word'], postData['team_id']
@@ -187,17 +191,20 @@ class SlackResponder(object):
 			postData['uLevel'] = self.userLevel['none'] # 0
 		
 		request = matchText[0]
+		logger.debug("Request: " + str(request))
 		
 		if not isinstance(request, basestring):
-			if len(request) == 2:
+			if len(request) >= 2:
 				trigger  = request[0]
 				argument = request[1]
+				logger.debug("Trigger: " + trigger)
+				logger.debug("Argument: " + argument)
 				# Third value for a user class (for .add)
 				if trigger == ".add":
 					try:
 						userClass = k[2]
 					except:
-						userClass = ""
+						userClass = "admin"
 				elif trigger == ".del":
 					try:
 						if k[2].lower() in ("yes", "true", "t", "1"):
@@ -207,32 +214,43 @@ class SlackResponder(object):
 					except:
 						hideUser = False
 
-				if trigger == ".balance":
+				if postData['uLevel'] < 1:
+					return "<@" + postData['user_id'] + ">: You are not an authorised user."
+				# Status update?
+				if trigger == ".status":
+					return self.PostStatusUpdate(postData, argument)
+				# Is the owner trying to be added or deleted?
+				elif trigger == ".add" or trigger == ".del":
+					#if str(argument) == str(botData.owner_id and postData['user_id'] == botData.owner_id):
+					#	return "<@" + postData['user_id'] + ">: Cannot perform actions on bot owner."
+					if trigger == ".add":
+						logger.debug("JSON data: " + json.dumps(postData, indent=4))
+						logger.debug("User to act on: " + argument)
+						logger.debug("Class to set user at: " + userClass)
+						return self.PromoteUser(postData, argument, userClass)
+					elif trigger == ".del": # Removes from either list
+						return self.DemoteUser(postData, argument, hideUser)
+				# Show/hide condition?
+				elif trigger == ".show" or trigger == ".hide":
+					if trigger == ".show":
+						return self.ShowUserPosts(postData, argument)
+					elif trigger == ".hide":
+						return self.HideUserPosts(postData, argument)
+				# User details?
+				elif trigger == ".name" or trigger == ".image" or trigger == ".email":
+					if trigger == ".name":
+						return self.UpdateUserInfo(postData, "name", argument)
+					elif trigger == ".email":
+						return self.UpdateUserInfo(postData, "email", argument)
+					elif trigger == ".image":
+						return self.UpdateUserInfo(postData, "image", argument)
+				elif trigger == ".balance":
 					return "Balance for address " + argument + ": " + str(self.GetBalance(argument)) + " SJCX"
 				elif trigger == ".twitter":
 					self.SetupJson()
 					self.botJson['twitter'][postData['user_id']] = argument
 					self.SaveJson()
-					return "<@" + jpostData['user_id'] + ">: Your twitter handle is now '" + argument + "'."
-				elif trigger == ".status" or trigger == ".add" or trigger == ".del" or trigger == ".show" or trigger == ".hide":
-					if postData['uLevel'] < 1:
-						return "<@" + postData['user_id'] + ">: You are not an authorised user."
-					# Status update?
-					if trigger == ".status":
-						return self.PostStatusUpdate(postData, argument)
-					# Is the owner trying to be added or deleted?
-					elif trigger == ".add" or trigger == ".del":
-						if str(argument) == str(botData.owner_id):
-							return "<@" + jsonData['user_id'] + ">: Cannot perform actions on bot owner."
-						if trigger == ".add":
-							return self.PromoteUser(postData, argument, userClass)
-						elif trigger == ".del": # Removes from either list
-							return self.DemoteUser(postData, argument, hideUser)
-					elif trigger == ".show" or trigger == ".hide":
-							if trigger == ".show":
-								return self.ShowUserPosts(postData, argument)
-							elif trigger == ".hide":
-								return self.HideUserPosts(postData, argument)
+					return "<@" + postData['user_id'] + ">: Your twitter handle is now '" + argument + "'."
 				else:
 					return "No response to give!"
 		elif request == ".list":
@@ -259,9 +277,9 @@ class SlackResponder(object):
 
 	def PostStatusUpdate(self, user, text):
 		''' Creates a status update for a user, refreshes template.
-		:param user: json dict representing the user data
-		:param text: The status text the user wishes to post
-		:return: String to send back to the user
+		@param user: json dict representing the user data
+		@param text: The status text the user wishes to post
+		@return: String to send back to the user
 		'''
 		self.SetupJson()
 		user['text'] = text
@@ -288,15 +306,17 @@ class SlackResponder(object):
 	def PromoteUser(self, user, subject, level):
 		''' Promotes a user to a higher level so they can post updates, or at
 		superuser level they can promote other users to update posting level.
-		:param user: json dict representing the user data
-		:param subject: The user to act on
-		:param level: int user level to promote to, 1=user, 2=superuser
-		:return: String to send back to the user.
+		@param user: json dict representing the user data
+		@param subject: The user to act on
+		@param level: int user level to promote to, 1=user, 2=superuser
+		@return: String to send back to the user.
 		'''
+		logger = logging.getLogger("SlackBot")
 		self.SetupJson()
+		logger.debug(json.dumps(user, indent=4))
 		if level == "superusers":
 			if user['uLevel'] >= 3:
-				if subject in self.botJson['superusers']:
+				if subject in self.botJson['superuser']:
 					return "<@" + user['user_id'] + ">: Action not needed."
 				self.botJson['superusers'].append(subject)
 				self.SaveJson()
@@ -317,10 +337,10 @@ class SlackResponder(object):
 	def DemoteUser(self, user, subject, hide):
 		''' Demotes a user so they won't be allowed to post updates. Optional hide posts.
 		Doesn't matter what level they are, they're gone.
-		:param user: json dict representing the user data
-		:param subject: The user to act upon
-		:param hide: Bool to hide posts by the user at the same time
-		:return: String to send back to the user
+		@param user: json dict representing the user data
+		@param subject: The user to act upon
+		@param hide: Bool to hide posts by the user at the same time
+		@return: String to send back to the user
 		'''
 		self.SetupJson()
 		if subject in self.botJson['admins']:
@@ -337,9 +357,9 @@ class SlackResponder(object):
 
 	def HideUserPosts(self, user, subject):
 		''' Hide posts by a user, refreshes template without that users posts.
-		:param user: json dict representing the user data
-		:param subject: The user to act upon
-		:return: String to send back to the user
+		@param user: json dict representing the user data
+		@param subject: The user to act upon
+		@return: String to send back to the user
 		''' 
 		self.SetupJson()
 		if user['uLevel'] >= 1:
@@ -353,9 +373,9 @@ class SlackResponder(object):
 	
 	def ShowUserPosts(self, user, subject):
 		''' Show posts by a user, refreshes template with that users posts.
-		:param user: json dict representing the user data
-		:param subject: The user to act upon
-		:return: String to send back to the user
+		@param user: json dict representing the user data
+		@param subject: The user to act upon
+		@return: String to send back to the user
 		'''
 		self.SetupJson()
 		if user['uLevel'] >= 1:
@@ -366,11 +386,40 @@ class SlackResponder(object):
 			self.OutputTemplate()
 			return "<@" + user['user_id'] + ">: Updates by <@" + subject + "> are now seen.\nTemplate refreshed."
 
+
+	def UpdateUserInfo(self, user, userValue, text):
+		''' Update user information so status updates work properly.
+		@param user: json dict representing the user data
+		@param userValue: The user information to update
+		@param text: The new value - a name, email address or image url.
+		@return: String to send back to the user
+		'''
+		self.SetupJson()
+		text = text.replace("'", "").replace("\\", "") # Bit of anti-xss
+		if userValue == "name":
+			self.botJson['users'][user['user_id']]['name'] = text
+			self.SaveJson()
+			return "<@" + user['user_id'] + ">: Your name has been updated.\nUse '.regen' to refresh the template."
+		elif userValue == "email":
+			gravatar_url = "http://www.gravatar.com/avatar/" + hashlib.md5(text.lower()).hexdigest() + "?"
+			gravatar_url += urllib.urlencode({'d':"http://storj.sdo-srv.com/storjlogo.jpg", 's':"72"})
+			self.botJson['users'][user['user_id']]['email'] = text
+			self.botJson['users'][user['user_id']]['image'] = gravatar_url
+			self.SaveJson()
+			response  = "<@" + user['user_id'] + ">: Your email address has been updated.\nI also updated the image url "
+			response += "with gravatar based on that email. If you would prefer to override that, use \".image <url>\" instead.\n"
+			response += "Use \".regen\" to refresh the template."
+			return response
+		elif userValue == "image":
+			self.botJson['users'][user['user_id']]['image'] = text
+			self.SaveJson()
+			return "<@" + user['user_id'] + ">: Your image url has been updated.\nUse '.regen' to refresh the template."
+	
 		
 	def UndoPost(self, user):
 		''' Undo a post a user has made.
-		:param user: json dict representing the user data
-		:return: String to send back to the user
+		@param user: json dict representing the user data
+		@return: String to send back to the user
 		'''
 		self.SetupJson()
 		self.botJson['updates'][user['user_id']] = self.botJson['undo'][user['user_id']]
@@ -378,6 +427,7 @@ class SlackResponder(object):
 		self.OutputTemplate()
 		response = "<@" + user['user_id'] + ">: I have undone your last update and refreshed the template."
 		return response
+
 
 	def AdminList(self, user):
 		admins = []
@@ -387,10 +437,11 @@ class SlackResponder(object):
 		final = ", ".join(admins)
 		return "Approved posters: " + final
 
+
 	def GetUser(self, user_id):
 		''' Fetch user information
-		:param user_id: user id to fetch info for
-		:return: bool True
+		@param user_id: user id to fetch info for
+		@return: bool True
 		'''
 		req = self.client._make_request('users.info', {'user': user_id})
 		self.botJson['users'][req['user']['id']] = req['user']
@@ -399,8 +450,8 @@ class SlackResponder(object):
 		
 	def GetBalance(self, address):
 		''' Fetch user sjcx balance
-		:param address: sjcx address
-		:return: balance string
+		@param address: sjcx address
+		@return: balance string
 		'''
 		test = requests.get("http://api.blockscan.com/api2?module=address&action=balance&asset=SJCX&btc_address=" + address).json()
 		if test['status'] == "success":
@@ -409,8 +460,8 @@ class SlackResponder(object):
 
 	def GetExRate(self, exchange):
 		'''Exchange rate fetcher.
-		:param exchange: What exchange to fetch data for.
-		:return: balance string
+		@param exchange: What exchange to fetch data for.
+		@return: balance string
 		'''
 		if exchange == "melotic":
 			rate = requests.get("https://www.melotic.com/api/markets/sjcx-btc/ticker", verify=False).json()
@@ -459,14 +510,13 @@ class SlackResponder(object):
 				dick = self.botJson['users'][user_id]['profile']['real_name']
 				print "Cannot process entry for " + dick + ", does not have twitter account set up."
 				continue
-			twitter = "https://twitter.com/" + self.botJson['twitter'][user_id]
 			
 			tdata.append({
 				"text": str(self.botJson['updates'][key]['text'].encode("utf-8")),
-				"name": self.botJson['users'][user_id]['profile']['real_name'],
-				"image": self.botJson['users'][user_id]['profile']['image_72'],
-				"twitter": twitter,
-				"email": self.botJson['users'][user_id]['profile']['email'],
+				"name": self.botJson['users'][user_id]['name'],
+				"image": self.botJson['users'][user_id]['image'],
+				"twitter": self.botJson['users'][user_id]['twitter'],
+				"email": self.botJson['users'][user_id]['email'],
 				"ts": datetime.datetime.fromtimestamp(float(ts)).strftime('%Y-%m-%d %H:%M:%S')
 			})
 
@@ -475,6 +525,25 @@ class SlackResponder(object):
 			with open(botData.output_file, 'w') as template_out:
 				template_out.write(template(users=tdata))
 				template_out.close()
+	
+	
+	def ConvertDetails(self):
+		self.SetupJson()
+		output = {}
+		
+		for key, val in self.botJson['updates'].iteritems():
+			output[key] = {
+				"name": self.botJson['users'][key]['profile']['real_name'],
+				"image": self.botJson['users'][key]['profile']['image_72'],
+				"email": self.botJson['users'][key]['profile']['email'],
+				"twitter": "https://twitter.com/" + self.botJson['twitter'][key]
+			}
+			self.botJson['users'][key] = output[key]
+			
+		self.SaveJson()
+			
+		print json.dumps(output, indent=4)
+	
 
 
 # Backup System
